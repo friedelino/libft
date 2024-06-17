9b79121aac690caa605528b01f8e59b50bd59dda
frido <frido@delta>
2024-06-17 17:42:56 +0200

    fix ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,24 +6,30 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 08:43:54 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/06/10 10:36:07 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/06/17 17:41:37 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// 1str try error: not returning dest when src == NULL
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 	char	*ds;
 	char	*sr;
 
-	if (!dest || !src)
+	if (!dest)
 		return (NULL);
-	ds = (char *) dest;
-	sr = (char *) src;
-	i = -1;
-	while (++i < n)
+	if (!src)
+		return (dest);
+	ds = (char *)dest;
+	sr = (char *)src;
+	i = 0;
+	while (i < n)
+	{
 		ds[i] = sr[i];
+		i++;
+	}
 	return (dest);
 }
