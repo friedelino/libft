e42bf160969228a9207b12fac3f31c385fe3f758
frido <frido@delta>
2024-06-17 17:48:37 +0200

    add comment to ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 19:58:57 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/06/16 20:12:46 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/06/17 17:48:26 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 // 2nd if: empty list given as NULL
 // always ensuring that newend->next == NULL
+// ...francinette did not like it.. but moulinette does. so i keep it.
 void	ft_lstadd_back(t_list **head, t_list *newend)
 {
 	t_list	*oldlast;
